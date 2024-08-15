0a8839296fadf6be995e7219ecde6f49ecf9f2b8
Je5514 <sreelakshminj@gmail.com>
2024-08-15 18:14:23 +0530

    header updated

--- source/scripts/site/main.js
+++ source/scripts/site/main.js
@@ -2,6 +2,8 @@
 $(window).on('load', function () {
     //page loading
     $('body').addClass('is-loaded');   
+    menuBurger();
+    openMenusub();
     AOS.init();  
 }); 
 
@@ -22,81 +24,16 @@ if((navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 0) || naviga
 }
 
 // Site related functions
-
-//define a function 
-function example_function() { 
-    console.log("function called") 
-} 
-
-//call the function 
-example_function();
-
-//menu toggle
-window.addEventListener("DOMContentLoaded", (event) => {
-  const navToggle= document.querySelector('#Toggle');
-  const nav = document.querySelector('.menu-items');
-  navToggle.addEventListener("click",()=>{
-      nav.classList.toggle('menu-open')
-  });
-});
-
-window.addEventListener("DOMContentLoaded", (event) => {
-const navShop = document.querySelector('#shopbar');
-navShop.addEventListener("click", () => {
-    alert("You have no items in your shopping cart. Please add cart");
-});
-});
-
-//back to top button
-window.onscroll = function() {
-    scrollFunction();
-  };
-  
-  function scrollFunction() {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-    
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      scrollToTopBtn.style.display = "block";
-    } else {
-      scrollToTopBtn.style.display = "none";
-    }
-    // const blur = document.getElementById('video');
-    
+function menuBurger(){
+  if($('.burger').length > 0){
+      $('.burger').on('click', function(e){
+          if($(this).hasClass('is-active')){
+            $(this).removeClass('is-active');
+            $('ul').removeClass('is-active');
+          }else{
+            $(this).addClass('is-active');
+            $('ul').addClass('is-active');
+          }
+      })
   }
-  
-  function scrollToTop() {
-     window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }
-
-  //show password
-  function mypassword() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
-
-  $(document).ready(function() {
-    $(window).scroll(function() {
-      var scrollPosition = $(this).scrollTop();
-      var video = $('#background-video');
-  
-      if (scrollPosition > 0) {
-        video.addClass('video--blur');
-      } else {
-        video.removeClass('video--blur');
-      }
-    });
-  });
-
-
-  
-  
-
-
-  
+}
